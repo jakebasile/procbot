64a8432bb5852c47eae5fe8aa400a43833037ff6
Jake Basile <me@jakebasile.com>
2013-07-07 20:55:04 -0500

    Updated URL in README.md. Bumped version.

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup
 setup(
     name='procbot',
     description='Process Based Chatbot',
-    version='0.1-dev',
+    version='0.1',
     author='Jake Basile',
+    url='http://github.com/jakebasile/procbot',
     scripts=[
         'procbot.py',
     ],
